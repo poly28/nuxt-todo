31ebd83825a6831798fdc0e248d8ba5f17c19808
poly28 <horimasa0328@gmail.com>
2021-06-15 22:39:49 +0900

    initial commit

---
+++ pages/todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      name: "",
+      done: false
+    };
+  },
+  created: function() {
+    this.$store.dispatch("todos/init");
+  },
+  methods: {
+    add() {
+      this.$store.dispatch("todos/add", this.name);
+    }
+  }
+};
+</script>
